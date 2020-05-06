b7fb3f4a893c228c23534d7bb6a3ab374b45245c
joshuaabrookuk <joshuabrookuk@gmail.com>
2020-05-06 11:04:31 +0000

    Updatelist of students

--- directory.rb
+++ directory.rb
@@ -1,3 +1,4 @@
+# first we print a list of all students 
 puts "The students of Villains Academy"
 puts "-------------"
 puts "Dr. Hannibal Lecter"
@@ -11,3 +12,10 @@ puts "Freddy Krueger"
 puts "The Joker"
 puts "Joffrey Baratheon"
 puts "Norman Bates"
+# then we print the total number of students 
+print "Overall, we have "
+print 11
+print " great students"
+#it’s important that print() doesn’t add new line characters
+
+# print "\nI\nam\ntesting\nback slash + n \nto break a string\nthis code was written on one line\n"
