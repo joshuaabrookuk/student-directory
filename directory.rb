27a6dd1abe057b5b6f6fceffa79e555e59b56c2d
joshuaabrookuk <joshuabrookuk@gmail.com>
2020-05-08 19:25:14 +0000

    Added multiple enteries, empty values have a default value, values are now symbols, user can correct typos

--- directory.rb
+++ directory.rb
@@ -1,4 +1,4 @@
-students = [
+Addstudents = [
     {name: "Dr. Hannibal Lecter", cohort: :november, origin: :Lithuania},
     {name: "Darth Vader" , cohort: :november, origin: :Tatooine},
     {name: "Nurse Ratched", cohort: :november, origin: :America},
@@ -34,23 +34,92 @@ def print_footer(names)
 end 
 
 def input_students
+  students = []
+
+  puts "Type 'Add' or 'View' "
+  choice = gets.chomp
+  
+  while choice == "Add" 
     puts "Please enter the names of the students"
     puts "To finish, hit return twice"
-    students = []
+    
     name = gets.chomp.capitalize 
+
     while !name.empty? do
-        if name[0] != "S" && name.length < 12
-            students << {name: name, cohort: :november, origin: :here}
-            puts "Now we have #{students.count} students"
-            name = gets.chomp.capitalize  
-        else
-            puts "Please enter the names of the students"
-            puts "To finish, hit return twice"
-            name = gets.chomp.capitalize 
-        end 
+      if name[0] != "S" && name.length < 12
+        puts "#{name}: Are you happy with this name? Y/N"
+        answer = gets.chomp.upcase
+        if answer == "Y" 
+          break 
+        elsif answer == "N"
+          puts "Please enter the names of the students"
+          puts "To finish, hit return twice"
+          name = gets.chomp.capitalize
+        end
+      else
+        puts "Please enter the names of the students"
+        puts "To finish, hit return twice"
+        name = gets.chomp.capitalize 
+      end 
     end 
-    students
-end
+
+    if name == ""
+      name = :default_name
+    end 
+
+    puts "Please enter the cohort for #{name}"
+    
+    cohort = gets.chomp
+      
+    while !cohort.empty? do 
+      puts "#{cohort}: Are you happy with this cohort? Y/N"
+      answer = gets.chomp.upcase
+      if answer == "Y"
+        break 
+      elsif answer == "N"
+        puts "Please enter the cohort for #{name}"
+        puts "To finish, hit return twice"
+        cohort = gets.chomp
+      end
+    end
+
+    if cohort == ""
+      cohort = :default_cohort
+    end 
+
+    puts "Please enter the origin for #{name}"
+    
+    origin = gets.chomp
+      
+    while !origin.empty? do 
+      puts "#{origin}: Is this origin coreect? Y/N"
+      answer = gets.chomp.upcase
+      if answer == "Y"
+        break
+      elsif answer == "N"
+        puts "Please enter the origin for #{name}"
+        puts "To finish, hit return twice"
+        origin = gets.chomp 
+      end
+    end
+
+    if origin == ""
+      origin = :default_origin
+    end
+
+    students << {name: name.to_sym, cohort: cohort.to_sym, origin: origin.to_sym} 
+    puts "Now we have #{students.count} students"
+
+    puts "'Add' or 'View' "
+    choice = gets.chomp
+  end
+  
+  while choice == "View"
+    break
+  end 
+  
+students
+end   
 
 students = input_students
 print_header 
