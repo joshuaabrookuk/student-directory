5efa4f98030d5a20c0a4c500d8df6b9ad39e54f5
joshuaabrookuk <joshuabrookuk@gmail.com>
2020-05-09 11:28:08 +0000

    Updated grammar for singular and plural students, fresh dir

--- directory.rb
+++ directory.rb
@@ -1,4 +1,4 @@
-Addstudents = [
+students = [
     {name: "Dr. Hannibal Lecter", cohort: :november, origin: :Lithuania},
     {name: "Darth Vader" , cohort: :november, origin: :Tatooine},
     {name: "Nurse Ratched", cohort: :november, origin: :America},
@@ -15,36 +15,19 @@ Addstudents = [
 def input_students
   puts "Please enter the names of the students"
   puts "To finish, just hit return twice"
-  # create an empty array
   students = []
-  # get the first name
   name = gets.chomp
-  # while the name is not empty, repeat this code
   while !name.empty? do
-    # add the student hash to the array
     students << {name: name, cohort: :november}
-    puts "Now we have #{students.count} students"
-    # get another name from the user
+    if students.length == 1
+      puts "Now we have #{students.count} student"
+    else
+      puts "Now we have #{students.count} students"
+    end 
     name = gets.chomp
   end
-  # return the array of students
   students
 end
-
-def students_by_cohort(students)
-  
-  students_by_cohort = {}
-  
-  students.each do |student|
-    cohort = student[:cohort]
-
-  if students_by_cohort[cohort] == nil
-  students_by_cohort[cohort] = []
-  end
-  students_by_cohort[cohort].push(student[:name])
-  end
-  p students_by_cohort
-end 
 
 def print_header
   puts "The students of Villains Academy"
@@ -58,12 +41,14 @@ def print(students)
 end
 
 def print_footer(students)
-  puts "Overall, we have #{students.count} great students"
+  if students.length == 1
+    puts puts "Overall, we have #{students.count} great student"
+  else
+    puts "Overall, we have #{students.count} great students"
+  end 
 end
 
 students = input_students
-#nothing happens until we call the methods
 print_header
-# print(students)
-students_by_cohort(students)
+print(students)
 print_footer(students)
