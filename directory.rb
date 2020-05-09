cb66421fa4ce44e08d016c5d3df49f8dd581f6f1
joshuaabrookuk <joshuabrookuk@gmail.com>
2020-05-09 18:24:35 +0000

    Added save to file functionality

--- directory.rb
+++ directory.rb
@@ -11,12 +11,47 @@ def input_students
   end
 end
 
+def interactive_menu
+  loop do
+    print_menu
+    process(gets.chomp)
+  end
+end
+
+def print_menu
+  puts "1. Input the students"
+  puts "2. Show the students"
+  puts "3. Save the list to students.csv"
+  puts "9. Exit"
+end
+
+def show_students
+  print_header
+  print_student_list
+  print_footer
+end
+
+def process(selection)
+  case selection
+  when "1"
+    input_students
+  when "2"
+    show_students
+    when "3"
+  save_students
+  when "9"
+    exit
+  else
+    puts "I don't know what you meant, try again"
+  end
+end
+
 def print_header
   puts "The students of Villains Academy"
   puts "-------------"
 end
 
-def print_students_list
+def print_student_list
   @students.each do |student|
     puts "#{student[:name]} (#{student[:cohort]} cohort)"
   end
@@ -26,39 +61,16 @@ def print_footer
   puts "Overall, we have #{@students.count} great students"
 end
 
-def interactive_menu
-  loop do
-  print_menu
-    process(gets.chomp)
+def save_students
+  file = File.open("students.csv", "w")
+  @students.each do |student|
+    student_data = [student[:name], student[:cohort]]
+    csv_line = student_data.join(",")
+    file.puts csv_line
   end
+  file.close
 end
 
-def print_menu
-  puts "1. Input the students"
-  puts "2. Show the students"
-  puts "9. Exit"
-end
 
-def process(selection)
-  case selection
-    when "1"
-      input_students
-    when "2"
-      show_students
-    when "9"
-      exit
-    else
-      puts "I don't know what you mean, try again"
-  end
-end
-
-def show_students
-  print_header
-  print_students_list
-  print_footer
-end
 
 interactive_menu
-print_header
-print_students_list
-print_footer
