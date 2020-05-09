bb0811f2599e494ae203e1140156c303787d7ce6
joshuaabrookuk <joshuabrookuk@gmail.com>
2020-05-09 15:09:36 +0000

    List wont print if no students named, fresh dir

--- directory.rb
+++ directory.rb
@@ -1,24 +1,24 @@
 def input_students
   puts "Please enter the names of the students"
   puts "To finish, just hit return twice"
-  # create an empty array
+
   students = []
-  # get the first name
-  name = gets .chomp
-  # while the name is not empty, repeat this code
-  while !name.empty? do
-  # add the student hash to the array
-  students << {name: name, cohort: :november}
-  puts "Now we have #{students.count} students"
-  # get another name from the user
+
   name = gets.chomp
+
+  while !name.empty? do
+
+    students << {name: name, cohort: :november}
+    puts "Now we have #{students.count} students"
+
+    name = gets.chomp
   end
-  # return the array of students
   students
 end
 
+
 def print_header
-  puts "The students of my cohort at Makers Academy"
+  puts "The students of Villains Academy"
   puts "-------------"
 end
 
@@ -29,11 +29,14 @@ def print(students)
 end
 
 def print_footer(students)
-  puts "Overall, we have {students.count} great students"
+  puts "Overall, we have #{students.count} great students"
 end
 
-
 students = input_students
-print_header
-print(students)
-print_footer(students)
+if students == [] 
+  puts "no list"
+else
+  print_header
+  print(students)
+  print_footer(students)
+end 
