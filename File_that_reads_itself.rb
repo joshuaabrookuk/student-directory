cd1fba325d0518d4a792c805cd3798bcd6606bef
joshuaabrookuk <joshuabrookuk@gmail.com>
2020-05-10 10:59:59 +0000

    File that reads it's own source code

---
+++ File_that_reads_itself.rb
@@ -0,0 +1,22 @@
+print File.read(__FILE__)
+
+#So I copy and pasted this quine of the internet
+
+# Originally I had planned to try and make my own. 
+
+# The plan was to wrie a ruby program that saves then reads it's self
+
+
+# File.open("Untitled1.rb", 'r')
+
+# but thats not working nore is that 
+
+# it's 01:48am 10/05/2020 and I need sleep please
+
+# file = File.open("Untitled1.rb", 'r')
+# file.close
+# file = File.read("Untitled1.rb", 'r')
+# file.close
+
+# I think the imaprtant thing is that I understand the question 
+# ands that I know why "puts File.read(__FILE__)" is working
